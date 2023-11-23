f55ebc97aed88172087c0c8b9856a3df0b8ef309
Allan Soares <av343877@gmail.com>
2023-11-23 20:52:11 -0300

    ok
    edit

--- src/router/MainRouter.ts
+++ src/router/MainRouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as homeController from "../controllers/homeController";
 export const router = Router();
 
-router.get("/home", homeController.Home);
+router.get("/", homeController.Home);
 router.get("/dog", homeController.Dogs);
 router.get("/cat", homeController.Cats);
 router.get("/fish", homeController.Fishes);
